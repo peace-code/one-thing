d6d31963607417d33dc5277eeac6c0c38d4798a1
ohyeon <rest515@ufofactory.org>
2015-04-05 14:09:57 +0900

    thing show page

--- app/helpers/things_helper.rb
+++ app/helpers/things_helper.rb
@@ -2,4 +2,8 @@ module ThingsHelper
   def featured_image(thing)
     thing.featured_image || asset_path('things/default-image.png')
   end
+
+  def featured_image_url(thing)
+    request.protocol + request.host_with_port + featured_image(thing)
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def excerpt(text)
+    raw(truncate(strip_tags(text), length: 150))
+  end
+
+  def user_thumb(user)
+    asset_path('users/default-thumb.png')
+  end
 end
